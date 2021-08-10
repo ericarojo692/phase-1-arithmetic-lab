1f08606f9f2471760dbd381b19e55794c0f240ab
Erica Rojo <ericarojo692@yahoo.com>
2021-08-10 14:58:37 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+const num1 = 62
+const num2 = 1
+const multiply = num1 * num2
+
+
+const random = num1;
+Math.random(num1);
+
+const num3 = 8
+const num4 = 4
+const mod = num3 - num4 
+
+
+let max = 20
+Math.max(1, 2, 3, 4, 20);
